8e0edaa49feb782610c33162664ffd5ee6db005b
Nan Pena <peangtan.pena@gmail.com>
2020-07-03 14:47:01 -0400

    created sticker bar component with redux store

--- client/components/StickerBar.js
+++ client/components/StickerBar.js
@@ -1,12 +1,41 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {fetchStickers} from '../store/sticker'
 
-const StickerBar = () => {
-  return (
-    <div>
-      <h1> Stickers </h1>
-    </div>
-  )
+export class StickerBar extends React.Component {
+  componentDidMount() {
+    this.props.fetchStickers()
+  }
+
+  render() {
+    const stickers = this.props.stickers
+    console.log('sticker', stickers)
+    return (
+      <div id="sticker">
+        <h1> Stickers!!</h1>
+        {stickers.map(sticker => {
+          return (
+            <div key={sticker.id}>
+              {' '}
+              <img src={sticker.imgURL} />
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
-export default StickerBar
+const mapState = state => {
+  console.log('mapState', state)
+  return {
+    stickers: state.sticker.stickers
+  }
+}
+
+const mapDispatch = dispatch => ({
+  fetchStickers: () => dispatch(fetchStickers())
+})
+
+export default connect(mapState, mapDispatch)(StickerBar)
--- client/store/sticker.js
+++ client/store/sticker.js
@@ -6,8 +6,37 @@ import history from '../history'
 const GET_STICKERS = 'GET_STICKERS'
 
 // INITIAL STATE //
-const defaultSticker = {}
+const initialState = {
+  stickers: []
+}
 
 // ACTION CREATORS //
+const getStickers = stickers => ({
+  type: GET_STICKERS,
+  stickers
+})
 
-const gotStickers = () => {}
+// Thunk Creators //
+
+export const fetchStickers = () => async dispatch => {
+  try {
+    const {data} = await axios.get('/api/stickers')
+    dispatch(getStickers(data))
+    console.log('data', data)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+// Reducer //
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_STICKERS:
+      console.log('reducer mode', state, action)
+
+      return {...state, stickers: action.stickers}
+    default:
+      return state
+  }
+}
