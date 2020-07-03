1db04c68e85c43199bf0f3f8289bd269bafc3fea
Nan Pena <peangtan.pena@gmail.com>
2020-07-03 13:13:18 -0400

    create new branch

---
+++ client/components/StickerBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const StickerBar = () => {
+  return (
+    <div>
+      <h1> Stickers </h1>
+    </div>
+  )
+}
+
+export default StickerBar
---
+++ client/store/sticker.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+import history from '../history'
+
+// ACTION TYPES //
+
+const GET_STICKERS = 'GET_STICKERS'
+
+// INITIAL STATE //
+const defaultSticker = {}
+
+// ACTION CREATORS //
+
+const gotStickers = () => {}
