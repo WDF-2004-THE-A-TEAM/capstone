2996cec616dd495d319a835e796ce4156a9fddae
Linda Eng <90lineng@gmail.com>
2020-07-03 11:40:24 -0400

    added seed

--- script/seed.js
+++ script/seed.js
@@ -1,44 +1,110 @@
-'use strict'
-
 const db = require('../server/db')
-const {User} = require('../server/db/models')
+const {User, Story, Page, Sticker} = require('../server/db/models')
 
-async function seed() {
-  await db.sync({force: true})
-  console.log('db synced!')
+const seedUser = [
+  {
+    name: 'Dog guy',
+    email: 'doggo@gmail.com',
+    password: 'abc123',
+    avatar: '',
+    salt: 'ab246',
+    googleId: 'googleString123',
+    isLoggedIn: true
+  },
+  {
+    name: 'Pizza guy',
+    email: 'pizza@gmail.com',
+    password: 'pepperon14lyfe',
+    avatar: '',
+    salt: '9876',
+    googleId: 'gzsee3',
+    isLoggedIn: false
+  },
+  {
+    name: 'Hunter guy',
+    email: 'hunter@cuny.edu',
+    password: 'college',
+    avatar: '',
+    salt: 'googlyeyes',
+    googleId: 'greatest',
+    isLoggedIn: false
+  }
+]
+const seedStory = [
+  {
+    title: 'Three little pigs',
+    author: 'Nan P',
+    coverImage:
+      'https://cdn.shopify.com/s/files/1/0278/9759/products/Three_Litte_Pigs_Box_Product_Front_-_Shopify_CM_2048x.jpg?v=1574272985'
+  },
+  {
+    title: 'Codouroy',
+    author: 'Linda E',
+    coverImage:
+      ' https://prodimage.images-bn.com/pimages/9780140501735_p0_v3_s550x406.jpg'
+  },
+  {
+    title: 'Where the wild things are ',
+    author: 'Claudia',
+    coverImage:
+      'https://images-na.ssl-images-amazon.com/images/I/71eczBv1C5L._AC_SL1001_.jpg'
+  }
+]
+const seedPage = [
+  {
+    imageURL:
+      'https://lh3.googleusercontent.com/proxy/_t50bNKNBV12lPLGS43V_CQSIgmWYLKBpKuBOhYMcduULqkPtuVkRlcnLwHUgF_t1lGDDalrRj1Lk0uXUaxD7YA-bPiOAilYjjpmDaZegKmVZl48XbEKjz5KUrXmNo07N5jTaWDaQ8YOjMj6xHFf'
+  },
+  {
+    imageURL:
+      'https://www.south-african-homeschool-curriculum.com/wp-content/uploads/2013/06/The-Herd-Boy.jpg'
+  },
+  {
+    imageURL:
+      'https://mir-s3-cdn-cf.behance.net/project_modules/1400/4f5b0b64115393.5ac74bf1aad22.jpg'
+  }
+]
+const seedSticker = [
+  {
+    imageURL:
+      'https://i.pinimg.com/236x/39/b7/1d/39b71d7e538389db653f0fb24cf4dff8--jeep-stickers-mac-stickers.jpg'
+  },
+  {
+    imageURL:
+      'https://dejpknyizje2n.cloudfront.net/marketplace/products/single-small-flame-sticker-1540311449.661467.png'
+  },
+  {
+    imageURL:
+      'https://dejpknyizje2n.cloudfront.net/svgcustom/clipart/preview/lightning-bolt-513-14749-550x550.png'
+  },
+  {
+    imageURL: ''
+  }
+]
 
-  const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
-  ])
-
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
-}
-
-// We've separated the `seed` function from the `runSeed` function.
-// This way we can isolate the error handling and exit trapping.
-// The `seed` function is concerned only with modifying the database.
-async function runSeed() {
-  console.log('seeding...')
+const seed = async () => {
   try {
-    await seed()
-  } catch (err) {
-    console.error(err)
-    process.exitCode = 1
-  } finally {
-    console.log('closing db connection')
-    await db.close()
-    console.log('db connection closed')
+    await db.sync({force: true})
+    await User.bulkCreate(seedUser, {validate: true})
+    await Story.bulkCreate(seedStory, {validate: true})
+    await Page.bulkCreate(seedPage, {validate: true})
+    await Sticker.bulkCreate(seedSticker, {validate: true})
+  } catch (error) {
+    console.log('SOMETHING WENT WRONG WITH THE SEEDING: ', error)
   }
 }
 
-// Execute the `seed` function, IF we ran this module directly (`node seed`).
-// `Async` functions always return a promise, so we can use `catch` to handle
-// any errors that might occur inside of `seed`.
-if (module === require.main) {
-  runSeed()
-}
-
-// we export the seed function for testing purposes (see `./seed.spec.js`)
 module.exports = seed
+
+if (require.main === module) {
+  seed()
+    .then(() => {
+      console.log('Seeding success!')
+      db.close()
+    })
+    .catch(err => {
+      console.error('Oh noes! Something went wrong!')
+      console.error(err)
+      db.close()
+    })
+}
