b6371050845bc835e09b615faee629cc55d8eb8b
Claudia Saimbert <csaimbert@gmail.com>
2020-07-03 22:58:53 -0400

    ✅ : Added Test for Page Model

--- server/db/models/page.spec.js
+++ server/db/models/page.spec.js
@@ -1,1 +1,23 @@
-//working
+const {expect} = require('chai')
+const db = require('../index')
+const Page = db.model('page')
+
+describe('Page Model', () => {
+  let page
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+  afterEach(() => db.sync({force: true}))
+  it('has imageURL', async () => {
+    const newPage = await Page.create(page)
+    expect(newPage.imgURL).to.equal('public/LindaEng_Untitled_Artwork 5.png')
+  })
+
+  it('has a default imgURL if none specified', async () => {
+    delete Page.imgURL
+    const defaultType = await Page.create(page)
+    expect(defaultType.imgURL).to.equal(
+      'public/LindaEng_Untitled_Artwork 5.png'
+    )
+  })
+})
