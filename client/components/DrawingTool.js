5a1920a96d10c1f4a21e74c961ee588d1b438070
AliyaBest <aliyabest@gmail.com>
2020-07-08 12:19:46 -0400

    FEATURE: Drawing tool component

---
+++ client/components/DrawingTool.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Button from '@material-ui/core/Button'
+
+export default class DrawingTool extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.drawOnCanvas = this.drawOnCanvas.bind(this)
+  }
+
+  componentDidMount() {
+    this.props.canvas.isDrawingMode = false
+    // this.props.canvas.freeDrawingBrush.width = 15
+    // this.props.canvas.freeDrawingBrush.color = '#00aeff'
+  }
+
+  drawOnCanvas() {
+    this.props.canvas.isDrawingMode = !this.props.canvas.isDrawingMode
+    if (this.props.canvas.isDrawingMode) {
+      document.getElementById('draw-button').innerHTML = 'Draw Mode ON'
+    } else {
+      document.getElementById('draw-button').innerHTML = 'Draw Mode OFF'
+    }
+  }
+
+  render() {
+    return (
+      <Button
+        id="draw-button"
+        onClick={() => {
+          this.drawOnCanvas()
+        }}
+      >
+        Draw Mode OFF
+      </Button>
+    )
+  }
+}
