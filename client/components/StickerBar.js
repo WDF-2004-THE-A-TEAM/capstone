35818e0e6da304abe5fb786d206d769fa0b709a1
Claudia Saimbert <csaimbert@gmail.com>
2020-07-08 21:06:45 -0400

    💄: updating sticker image dimensions

--- client/components/StickerBar.js
+++ client/components/StickerBar.js
@@ -5,6 +5,7 @@ import {fetchStickers} from '../store/sticker'
 import {makeStyles} from '@material-ui/core/styles'
 import GridList from '@material-ui/core/GridList'
 import GridListTile from '@material-ui/core/GridListTile'
+import testImg from '../../public/images/stickers/banana.png'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -24,12 +25,15 @@ const StickerBar = props => {
   const stickers = props.stickers
   console.log('sticker', stickers)
   const addToCanvas = props.addToCanvas
+  //public/images/stickers/arch.png
 
   const classes = useStyles()
+  console.log(stickers)
   return (
     <div id="sticker" className={classes.root}>
       <GridList className={classes.gridList} cellHeight={160} col={3}>
         <h1> Stickers!!</h1>
+        <img src={testImg} />
         {stickers.map(sticker => {
           return (
             <GridListTile
@@ -38,7 +42,7 @@ const StickerBar = props => {
                 addToCanvas(sticker)
               }}
             >
-              <img src={sticker.imgURL} />
+              <img src={`${sticker.imgURL}`} width="100" height="auto" />
             </GridListTile>
           )
         })}
@@ -48,60 +52,3 @@ const StickerBar = props => {
 }
 
 export default StickerBar
-
-// export class StickerBar extends React.Component {
-//   constructor(props) {
-//     super(props)
-
-//     this.addToCanvas = this.addToCanvas.bind(this)
-//   }
-
-//   componentDidMount() {
-//     this.props.fetchStickers()
-//   }
-
-// addToCanvas(sticker) {
-//   console.log('clicked', sticker)
-//   const canvas = new fabric.Canvas('my-canvas')
-
-//   fabric.Image.fromURL(sticker.imgURL, img => {
-//     img.scale(0.2)
-//     img.set({left: 100, top: 100})
-//     canvas
-//       .add(img)
-//       .renderAll()
-//       .setActiveObject(img)
-//   })
-
-//   // console.log("image",image)
-// }
-
-//   render() {
-//     const stickers = this.props.stickers
-//     return (
-//       <div id="sticker">
-//         <h1> Stickers!!</h1>
-//         {stickers.map(sticker => {
-//           return (
-//             <div key={sticker.id} onClick={() => this.addToCanvas(sticker)}>
-//               <img src={sticker.imgURL} />
-//             </div>
-//           )
-//         })}
-//       </div>
-//     )
-//   }
-// }
-
-// const mapState = state => {
-//   console.log('mapState', state)
-//   return {
-//     stickers: state.sticker.stickers
-//   }
-// }
-
-// const mapDispatch = dispatch => ({
-//   fetchStickers: () => dispatch(fetchStickers())
-// })
-
-// export default connect(mapState, mapDispatch)(StickerBar)
