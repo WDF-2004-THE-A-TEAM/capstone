1ffc90e8e573fe7b2498df9aa556cccc956b64b6
Linda Eng <90lineng@gmail.com>
2020-07-14 11:16:55 -0400

    FEAT: Amazon routing

--- server/index.js
+++ server/index.js
@@ -70,6 +70,10 @@ const createApp = () => {
   // static file-serving middleware
   app.use(express.static(path.join(__dirname, '..', 'public')))
 
+  //AMAZON
+  const profile = require('./routes/api/profile')
+  app.use('/api/profile', profile)
+
   // any remaining requests with an extension (.js, .css, etc.) send 404
   app.use((req, res, next) => {
     if (path.extname(req.path).length) {
