93a3afcbda0124f641308da3b30317a95cd56121
Claudia Saimbert <csaimbert@gmail.com>
2020-07-17 03:07:42 -0400

    💄: increased padding on button group

--- client/components/ToolBar.js
+++ client/components/ToolBar.js
@@ -7,6 +7,7 @@ import TextTool from './TextTool'
 import DrawingTool from './DrawingTool'
 import StickerBar from './StickerBar'
 import Remove from './Delete'
+import Fade from '@material-ui/core/Fade'
 import Tooltip from '@material-ui/core/Tooltip'
 
 const useStyles = makeStyles(theme => ({
@@ -15,6 +16,10 @@ const useStyles = makeStyles(theme => ({
     '& > *': {
       margin: theme.spacing(1)
     }
+  },
+  buttonStyle: {
+    color: '#4F6D7A',
+    padding: 40
   }
 }))
 
@@ -28,8 +33,14 @@ const ToolBar = props => {
   return (
     <div className={classes.root}>
       <ButtonGroup orientation="vertical">
-        <Tooltip title="STICKERS" placement="right-start" arrow>
-          <Button>
+        <Tooltip
+          TransitionComponent={Fade}
+          TransitionProps={{timeout: 600}}
+          title="STICKERS"
+          placement="right-start"
+          arrow
+        >
+          <Button className={classes.buttonStyle}>
             <StickerBar
               canvas={props.canvas}
               stickers={props.stickers}
@@ -38,27 +49,56 @@ const ToolBar = props => {
           </Button>
         </Tooltip>
 
-        <Tooltip title="DRAW" placement="right-start" arrow>
-          <Button>
+        <Tooltip
+          TransitionComponent={Fade}
+          TransitionProps={{timeout: 600}}
+          title="DRAW"
+          placement="right-start"
+          arrow
+        >
+          <Button className={classes.buttonStyle}>
             <DrawingTool canvas={props.canvas} />
           </Button>
         </Tooltip>
 
-        <Tooltip title="TEXT" placement="right-start" arrow>
-          <Button>
+        <Tooltip
+          TransitionComponent={Fade}
+          TransitionProps={{timeout: 600}}
+          title="TEXT"
+          placement="right-start"
+          arrow
+        >
+          <Button className={classes.buttonStyle}>
             <TextTool canvas={props.canvas} />
           </Button>
         </Tooltip>
 
-        <Tooltip title="DELETE" placement="right-start" arrow>
-          <Button>
+        <Tooltip
+          TransitionComponent={Fade}
+          TransitionProps={{timeout: 600}}
+          title="DELETE"
+          placement="right-start"
+          arrow
+        >
+          <Button className={classes.buttonStyle}>
             <Remove canvas={props.canvas} />
           </Button>
         </Tooltip>
 
-        <Tooltip title="CLEAR CANVAS" placement="right-start" arrow>
-          <Button aria-label="delete" fontSize="large">
+        <Tooltip
+          TransitionComponent={Fade}
+          TransitionProps={{timeout: 600}}
+          title="CLEAR CANVAS"
+          placement="right-start"
+          arrow
+        >
+          <Button
+            className={classes.buttonStyle}
+            aria-label="delete"
+            fontSize="large"
+          >
             <ClearRoundedIcon
+              fontSize="large"
               onClick={() => {
                 clearEl()
               }}
