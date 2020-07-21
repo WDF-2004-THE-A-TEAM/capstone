f64450e866dfc31699eb8ccf88ec4d3f91e3adf3
Claudia Saimbert <csaimbert@gmail.com>
2020-07-21 01:50:23 -0400

    🚧 : Working on creating cloud image buttons for splash page

--- client/components/SplashPage.js
+++ client/components/SplashPage.js
@@ -3,10 +3,19 @@ import {makeStyles} from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import Box from '@material-ui/core/box'
 import splashPageImage from '../../public/splash_unicorn.png'
-
+import DrawButton from '../../public/draw_button.png'
+import SignUpButton from '../../public/become_a_member.png'
+import Avatar from '@material-ui/core/Avatar'
 import {Link} from 'react-router-dom'
 
 import Button from '@material-ui/core/Button'
+import ButtonBase from '@material-ui/core/ButtonBase'
+
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
 
 const useStyles = makeStyles(theme => ({
   hero: {
@@ -61,24 +70,20 @@ function SplashPage() {
           </Typography> */}
           <Box className={classes.buttonBox}>
             <Link to="/canvas">
-              <Button
-                className={classes.button}
-                variant="contained"
-                color="primary"
-                disableElevation
-              >
-                Draw
-              </Button>
+              <Card className={classes.root}>
+                <CardActionArea>
+                  <CardMedia component="img" height="140" image={DrawButton} />
+                </CardActionArea>
+              </Card>
             </Link>
             <Link to="/signup">
-              <Button
+              <ButtonBase
                 className={classes.button}
                 variant="contained"
                 color="primary"
                 disableElevation
-              >
-                Become a StoryTeller
-              </Button>
+                startIcon={<Avatar src={SignUpButton} />}
+              ></ButtonBase>
             </Link>
           </Box>
         </Box>
